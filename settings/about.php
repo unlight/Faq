37f9577a7aa7c99c1949e9e26147e17fd59083bc
S <unlights@gmail.com>
2011-08-16 13:39:11 +0400

    About

--- settings/about.php
+++ settings/about.php
@@ -1,14 +1,14 @@
 <?php if (!defined('APPLICATION')) exit();
 
 $ApplicationInfo['Faq'] = array(
-	'Description' => 'Frequently asked questions are listed questions and answers, all supposed to be commonly asked in some context, and pertaining to a particular topic.',
-	'Version' => '0.01',
+	'Description' => 'Frequently asked questions are listed questions and answers.',
+	'Version' => '0.02',
 	'Date' => 'Summer 2011',
 	'AllowEnable' => True,
 	'RequiredPlugins' => False,
 	'RequiredApplications' => False,
 	'Author' => 'Frostbite',
-	'AuthorUrl' => 'http://www.liandri-mining.com/',
+	'AuthorUrl' => 'http://www.br-serenity.info/',
 	'License' => 'X.Net'
 );
 
